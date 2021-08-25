5ea4dbdf3842e270dd076cd2a025bc847a9f3658
mbrsagor <mbrsagor@gmail.com>
2021-08-25 08:28:36 +0600

    New: project dockerization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import requests
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
@@ -19,6 +21,8 @@ class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
 @app.route('/')
